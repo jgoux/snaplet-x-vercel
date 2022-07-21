4d10ba1dce92c03d7b3367aebc189349a51f09ec
jgoux <hi@jgoux.dev>
2022-07-21 16:06:44 +0200

    test

--- scripts/ignore-build-step.mjs
+++ scripts/ignore-build-step.mjs
@@ -1,3 +1,16 @@
 import fetch from "node-fetch";
 
-console.log(process.env)
+const env = process.env;
+
+console.log({ env });
+
+if (process.env.VERCEL_ENV === "preview") {
+  const response = await fetch(`https://api.vercel.com/v13/deployments/${process.env.VERCEL_URL}`, {
+    headers: {
+      "Authorization": `Bearer ${process.env.VERCEL_ACCESS_TOKEN}`,
+    },
+  }).then(res => res.json());
+  console.log(response);
+}
+
+process.exit(0);
