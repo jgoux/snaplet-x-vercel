ac1b5d8a37d09d841c8ff61ab195895eb4619ad8
jgoux <hi@jgoux.dev>
2022-07-21 16:14:15 +0200

    only allow deploy hook in preview

--- scripts/ignore-build-step.mjs
+++ scripts/ignore-build-step.mjs
@@ -1,16 +1,15 @@
 import fetch from "node-fetch";
 
-const env = process.env;
-
-console.log({ env });
-
 if (process.env.VERCEL_ENV === "preview") {
-  const response = await fetch(`https://api.vercel.com/v13/deployments/${process.env.VERCEL_URL}`, {
+  const deployment = await fetch(`https://api.vercel.com/v13/deployments/${process.env.VERCEL_URL}`, {
     headers: {
       "Authorization": `Bearer ${process.env.VERCEL_ACCESS_TOKEN}`,
     },
   }).then(res => res.json());
-  console.log(response);
+
+  if (!deployment?.meta?.deployHookId) {
+    process.exit(0);
+  }
 }
 
-process.exit(0);
+process.exit(1);
